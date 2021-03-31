3de314755967efcf6af47714907dabe6f2baaf63
imran5644 <imi56444@gmail.com>
2021-04-01 01:04:07 +0800

    Add the createNav function to add the navbar menu

--- src/index.js
+++ src/index.js
@@ -1,18 +1,49 @@
 function createHeader(){
-const content = document.querySelector('#content');
-const header = document.createElement('header');
-header.classList.add('header');
+	const content = document.querySelector('#content');
+	const header = document.createElement('header');
+	header.classList.add('header');
 
-const restaurantName = document.createElement('h1');  
-restaurantName.classList.add('restaurant-name');
-restaurantName.textContent = 'Special Burgers';
+	const restaurantName = document.createElement('h1');  
+	restaurantName.classList.add('restaurant-name');
+	restaurantName.textContent = 'Special Burgers';
 
-header.appendChild(restaurantName);
-content.appendChild(header);
-console.log(header);
-return header;
-
+	content.appendChild(header);
+	header.appendChild(restaurantName);
+	header.appendChild(createNav());
+	return header;
 }
+
+function createNav() {
+  const navbar = document.createElement('nav');
+	navbar.classList.add('navbar');
+
+	const homebtn = document.createElement('button');
+	homebtn.classList.add('home-btn');
+	homebtn.textContent = 'Home';
+	homebtn.addEventListener('click', (e)=>{
+		if (e.target.classList.contains('active')) return;
+	})
+
+	const menubtn = document.createElement('button');
+	menubtn.classList.add('menu-btn');
+	menubtn.textContent = 'Menu';
+	menubtn.addEventListener('click', (e)=>{
+		if (e.target.classList.contains('active')) return;
+	})
+
+	const contactbtn = document.createElement('button');
+	contactbtn.classList.add('contact-btn');
+	contactbtn.textContent = 'Contact';
+	contactbtn.addEventListener('click', (e)=>{
+		if (e.target.classList.contains('active')) return;
+	})
+
+	navbar.appendChild(homebtn);
+	navbar.appendChild(menubtn);
+	navbar.appendChild(contactbtn);
+	return navbar;
+}
+
 
 window.onload = function() {
     createHeader();
