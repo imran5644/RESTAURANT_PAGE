a00d7fb58e0609b46a93d28ea221ba592788c81a
imran5644 <imi56444@gmail.com>
2021-04-01 00:23:28 +0800

    Add the Header with name

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-document.body.onload = createHeader;
-
 function createHeader(){
-const header = document.createElement("header");  
-header.innerHTML = "Special Burgers";                   
-document.body.appendChild(header);
+const content = document.querySelector('#content');
+const header = document.createElement('header');
+header.classList.add('header');
+
+const restaurantName = document.createElement('h1');  
+restaurantName.classList.add('restaurant-name');
+restaurantName.textContent = 'Special Burgers';
+
+header.appendChild(restaurantName);
+content.appendChild(header);
+console.log(header);
+return header;
+
 }
 
-
-
+window.onload = function() {
+    createHeader();
+  }
