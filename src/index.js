87b2aa3e52a3cfd905f49a8646ce7f43a551837d
imran5644 <imi56444@gmail.com>
2021-04-02 03:19:39 +0800

    Add the style and github link to the footer

--- src/index.js
+++ src/index.js
@@ -74,7 +74,20 @@ const createFooter = () => {
   const footer = document.createElement('footer')
 	footer.classList.add('footer');
 
-	footer.textContent = 'Developed with ♥ by Imran Yousaf';
+	
+	const copyright = document.createElement("p");
+	copyright.textContent = 'Developed with ♥ by Imran Yousaf';
+
+	const githubLink = document.createElement("a");
+    githubLink.href = "https://github.com/imran5644";
+
+    const githubIcon = document.createElement("i");
+    githubIcon.classList.add("fab");
+    githubIcon.classList.add("fa-github");
+    githubLink.appendChild(githubIcon);
+
+    footer.appendChild(copyright);
+    footer.appendChild(githubLink);
 
 	return footer;
 }
